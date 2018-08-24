b1d5e6134e0c97ab344c7d965119a561dff4e1a9
Benedicte Roussel <benedicte55555@yahoo.fr>
2018-08-24 17:09:58 +0200

    Come back to GridView

--- lib/home.dart
+++ lib/home.dart
@@ -11,6 +11,131 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+
+
+// GRID VIEW
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'model/data.dart';
+import 'model/product.dart';
+
+class HomePage extends StatelessWidget {
+  // TODO: Make a collection of cards (102)
+  List<Card> _buildGridCards(BuildContext context) {
+    List<Product> products = getProducts(Category.all);
+
+    if (products == null || products.isEmpty) {
+      return const <Card>[];
+    }
+
+    final ThemeData theme = Theme.of(context);
+    final NumberFormat formatter = NumberFormat.simpleCurrency(
+        locale: Localizations.localeOf(context).toString());
+
+    return products.map((product) {
+      return Card(
+        // TODO: Adjust card heights (103)
+        child: Column(
+          // TODO: Center items on the card (103)
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 18 / 11,
+              child: Image.asset(
+                product.assetName,
+                package: product.assetPackage,
+                // TODO: Adjust the box size (102)
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                child: Column(
+                  // TODO: Align labels to the bottom and center (103)
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    // TODO: Handle overflowing labels (103)
+                    Text(
+                      product == null ? '' : product.name,
+                      style: theme.textTheme.button,
+                      softWrap: false,
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 1,
+                    ),
+                    SizedBox(height: 4.0),
+                    Text(
+                      product == null ? '' : formatter.format(product.price),
+                      style: theme.textTheme.caption,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }).toList();
+  }
+
+  // TODO: Add a variable for Category (104)
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Return an AsymmetricView (104)
+    // TODO: Pass Category variable to AsymmetricView (104)
+    return Scaffold(
+      // TODO: Add app bar (102)
+      appBar: AppBar(
+        // TODO: Add buttons and title (102)
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            semanticLabel: 'menu',
+          ),
+          onPressed: () {
+            print('Menu button');
+          },
+        ),
+        title: Text('SHRINE'),
+        // TODO: Add trailing buttons (102)
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              semanticLabel: 'search',
+            ),
+            onPressed: () {
+              print('Search button');
+            },
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.tune,
+              semanticLabel: 'filter',
+            ),
+            onPressed: () {
+              print('Filter button');
+            },
+          ),
+        ],
+      ),
+      // TODO: Add a grid view (102)
+      body: GridView.count(
+          crossAxisCount: 2,
+          padding: EdgeInsets.all(16.0),
+          childAspectRatio: 8.0 / 9.0,
+          children: _buildGridCards(context)),
+    );
+  }
+}
+
+
+
+//ASYMMETRIC VIEW
+/*
 import 'package:flutter/material.dart';
 
 import 'model/data.dart';
@@ -54,9 +179,11 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+*/
 
 
 
+//GRID VIEW DRAFT
 
 /*
 import 'package:flutter/material.dart';
@@ -97,7 +224,7 @@ class HomePage extends StatelessWidget {
     );
 
     return cards;
-  }*//*
+  }*/ /*
 
   List<Card> _buildGridCards(BuildContext context) {
     List<Product> products = getProducts(Category.all);
@@ -113,13 +240,11 @@ class HomePage extends StatelessWidget {
     return products.map((product) {
       return Card(
         // TODO: Adjust card heights (103)
-        elevation: 0.0,
         child: Column(
           // TODO: Center items on the card (103)
           crossAxisAlignment: CrossAxisAlignment.center,
           */
-/*crossAxisAlignment: CrossAxisAlignment.start,*//*
-
+/*crossAxisAlignment: CrossAxisAlignment.start,*/ /*
 
           children: <Widget>[
             AspectRatio(
@@ -139,7 +264,7 @@ class HomePage extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.center,
 
                   */
-/*crossAxisAlignment: CrossAxisAlignment.start,*//*
+/*crossAxisAlignment: CrossAxisAlignment.start,*/ /*
 
                   children: <Widget>[
                     // TODO: Handle overflowing labels (103)
@@ -166,7 +291,7 @@ class HomePage extends StatelessWidget {
                     Text(
                       formatter.format(product.price),
                       style: theme.textTheme.body2,
-                    ),*//*
+                    ),*/ /*
 
                   ],
                 ),
@@ -224,7 +349,7 @@ class HomePage extends StatelessWidget {
       */
 /*body: Center(
         child: Text('You did it!'),
-      ),*//*
+      ),*/ /*
 
 
      */
@@ -258,7 +383,7 @@ class HomePage extends StatelessWidget {
           )
         ],
       ),
-*//*
+*/ /*
 
      */
 /* body: new GridView.count(
@@ -266,7 +391,7 @@ class HomePage extends StatelessWidget {
           padding: EdgeInsets.all(16.0),
           childAspectRatio: 8.0 / 9.0,
           children: _buildGridCards(10)
-      ),*//*
+      ),*/ /*
 
 
       body: GridView.count(
