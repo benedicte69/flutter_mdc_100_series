f1a8a27ffd4a07fa82799d4260c0d7bc136367ae
Benedicte Roussel <benedicte55555@yahoo.fr>
2018-08-24 14:33:09 +0200

    Add PrimaryColorOverride

--- lib/login.dart
+++ lib/login.dart
@@ -96,3 +96,18 @@ class _LoginPageState extends State<LoginPage> {
 }
 
 // TODO: Add PrimaryColorOverride (103)
+class PrimaryColorOverride extends StatelessWidget {
+  const PrimaryColorOverride({Key key, this.color, this.child})
+      : super(key: key);
+
+  final Color color;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      child: child,
+      data: Theme.of(context).copyWith(primaryColor: color),
+    );
+  }
+}
